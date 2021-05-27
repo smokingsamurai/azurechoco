984d76610c0042999ef80cf5a876ced7c849a193
Orlando de Cordova <24925896+smokingsamurai@users.noreply.github.com>
2021-05-27 12:41:50 +0100

    Update baseline.ps1

--- baseline.ps1
+++ baseline.ps1
@@ -1,5 +1,10 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install dotnetcore-sdk --version=3.1.401 -y
-choco install dotnet-sdk --version=5.0.201 -y
-choco install dotnet-5.0-windowshosting --version=5.0.0 -y
+cChocoInstaller installChoco
+  {
+    InstallDir = "c:\ProgramData\chocolatey"
+  }
+
+  cChocoPackageInstaller installGit
+  {
+     Name = "git"
+     DependsOn = "[cChocoInstaller]installChoco"
+  }
