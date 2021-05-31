492789d3470094d2035c532cbc3c76a59c92ee03
Orlando de Cordova <24925896+smokingsamurai@users.noreply.github.com>
2021-05-31 22:54:50 +0100

    Update baseline.ps1

--- baseline.ps1
+++ baseline.ps1
@@ -5,4 +5,4 @@ choco install dotnetcore-sdk --version=3.1.401 -y
 choco install dotnet-sdk --version=5.0.201 -y
 choco install dotnet-5.0-windowshosting --version=5.0.0 -y
 choco install googlechrome -y
-Add-Content -Path "C:\inetpub\wwwroot\Default.htm\" -Value "Response from"+  $($env:computername)
+Add-Content -Path "C:\inetpub\wwwroot\Default.htm\" -Value $($env:computername)
