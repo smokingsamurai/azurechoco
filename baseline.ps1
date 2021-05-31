fa840e7255b8779a4909ed225fcf06771f0c977e
Orlando de Cordova <24925896+smokingsamurai@users.noreply.github.com>
2021-05-31 22:11:13 +0100

    Update baseline.ps1

--- baseline.ps1
+++ baseline.ps1
@@ -1,8 +1,8 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Add-WindowsFeature Web-Server
 choco install dotnetcore-sdk --version=3.1.401 -y
 choco install dotnet-sdk --version=5.0.201 -y
 choco install dotnet-5.0-windowshosting --version=5.0.0 -y
 choco install googlechrome -y
-Add-WindowsFeature Web-Server
-Add-Content -Path "C:\inetpub\wwwroot\Default.htm\" -Value "Response from  $($env:computername)"
+Add-Content -Path "C:\inetpub\wwwroot\Default.htm\" -Value "Response from"+  $($env:computername)
